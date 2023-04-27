03fbcce22744525327701eb9c5650e15879525db
Emre Turan <emreee.t99@gmail.com>
2023-04-27 17:44:10 +0300

    make appointments

--- client/website/src/axios/axios.js
+++ client/website/src/axios/axios.js
@@ -1,8 +1,17 @@
 import axios from "axios"
 
 const HTTP = axios.create({
-    baseURL: "http://localhost:4097",
+    baseURL: "http://localhost:4096",
 })
 
 export const SignUp = async(formData) =>
     await HTTP.post("/student/signup", formData);
+
+export const Login = async(formData) =>
+    await HTTP.post("/student/signin", formData);
+
+export const DoctorLogin = async(formData) =>
+    await HTTP.post("/doctor/signin", formData);
+
+export const MakeAppointment = async(formData) =>
+    await HTTP.post("/student/MakeAppointment", formData);
